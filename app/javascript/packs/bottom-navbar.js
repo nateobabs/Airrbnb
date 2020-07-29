b00885361b3e12018a416cdae113dd9ff8b4b08b
Deepak Mahajan <deepakmahajan@gmail.com>
2020-07-29 23:42:05 +0530

    Airrbnb Clone

--- app/javascript/packs/bottom-navbar.js
+++ app/javascript/packs/bottom-navbar.js
@@ -1,5 +1,6 @@
-$(window).scroll(function() {
-  if(window.innerHeight + window.scrollY > document.body.clientHeight) {
+$(document).scroll(function() {
+  console.log(window.innerHeight, window.scrollY, document.body.clientHeight);
+  if(window.innerHeight + window.scrollY > document.body.clientHeight - 30) {
     document.getElementById('bottom-navbar').style.display='none';
   } else{
     document.getElementById('bottom-navbar').style.display='block';
