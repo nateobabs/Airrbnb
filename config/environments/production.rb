09e7d914667a2e9c4e594ec21d872c58c41b8ba5
Deepak Mahajan <deepakmahajan@gmail.com>
2020-07-29 20:43:47 +0530

    Airrbnb Clone

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,16 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "Airrbnb_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name:      ENV['SENDMAIL_USERNAME'],
+    password:       ENV['SENDMAIL_PASSWORD'],
+    domain:         ENV['MAIL_HOST'],
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
