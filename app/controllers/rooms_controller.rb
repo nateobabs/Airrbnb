887bfae9044f4f34855a274ddfaeac5f262561a6
Deepak Mahajan <deepakmahajan@gmail.com>
2020-07-31 16:32:16 +0530

    Airrbnb Clone

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,7 +1,7 @@
 class RoomsController < ApplicationController
 
   before_action :set_room, only: [:show, :edit, :destroy, :update]
-  before_action :authenticate_user!, except: [:show]
+  before_action :authenticate_user!, except: [:index]
   
   def index
     @rooms = current_user.rooms.all
